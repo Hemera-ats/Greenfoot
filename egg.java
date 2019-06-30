8c4a68b9d3dd1a380dd5a4f55e56ca10f6f08794
Hemera-ats <51821803+Hemera-ats@users.noreply.github.com>
2019-06-30 20:17:10 +0700

    Update egg.java

--- egg.java
+++ egg.java
@@ -1,11 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class lemon here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class egg extends benda
 {
     /**
