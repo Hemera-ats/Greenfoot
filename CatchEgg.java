534b993624058c8a7d64bd4e82a87510063d8fd8
Hemera-ats <51821803+Hemera-ats@users.noreply.github.com>
2019-06-30 20:13:03 +0700

    Update CatchEgg.java

--- CatchEgg.java
+++ CatchEgg.java
@@ -1,18 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class CatchEgg extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
     public CatchEgg()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
